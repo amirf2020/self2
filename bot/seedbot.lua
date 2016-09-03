58b8a8b655a43849fdb64ae28b95c545e554e9f5
amirf2020 <arminghafouri8869.8869@gmail.com>
2016-09-03 00:27:51 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {174410791,0,tonumber(our_id)},--Sudo users
+    sudo_users = {174410791,241188407,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
